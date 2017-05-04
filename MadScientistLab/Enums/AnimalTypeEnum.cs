a85664c1fb0a68d8c648db670137538350b4147c
Putzolu Adrien <Putzolu Adrien>
2017-05-04 12:03:39 +0200

    Add project files.

---
+++ MadScientistLab/Enums/AnimalTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace MadScientistLab.Enums
+{
+    public enum AnimalTypeEnum
+    {
+        Cat,
+        Lion,
+        Dog,
+        Wolf,
+        Mouse,
+        Rat,
+        tiger,
+        coyote,
+        hamster
+    }
+}
